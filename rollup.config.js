98ba2cbabea9f432e47a9ae415e0d4fb78d7227c
KairuiLiu <tclkr@live.com>
2024-04-16 19:15:35 +1200

    fix: deploy

--- rollup.config.js
+++ rollup.config.js
@@ -1,29 +1,32 @@
-import resolve from '@rollup/plugin-node-resolve';
-import commonjs from '@rollup/plugin-commonjs';
-import json from '@rollup/plugin-json';
-import typescript from '@rollup/plugin-typescript';
-import alias from '@rollup/plugin-alias';
-import path from 'path';
-import { dirname } from 'path';
+import fs from 'fs';
 import { fileURLToPath } from 'url';
+import path, { dirname } from 'path';
 import copy from 'rollup-plugin-copy';
+import del from 'rollup-plugin-delete';
+import json from '@rollup/plugin-json';
+import alias from '@rollup/plugin-alias';
+import terser from '@rollup/plugin-terser';
+import commonjs from '@rollup/plugin-commonjs';
+import resolve from '@rollup/plugin-node-resolve';
+import typescript from '@rollup/plugin-typescript';
+
+const dir = dirname(fileURLToPath(import.meta.url));
+const dev = process.env.NODE_ENV === 'DEV';
 
 export default {
   input: 'src/main.ts',
   output: {
     dir: 'dist',
     format: 'es',
-    sourcemap: true,
+    sourcemap: dev,
   },
   plugins: [
+    !dev && del({ targets: 'dist' }),
     alias({
       entries: [
         {
           find: '@',
-          replacement: path.resolve(
-            dirname(fileURLToPath(import.meta.url)),
-            'src',
-          ),
+          replacement: path.resolve(dir, 'src'),
         },
       ],
     }),
@@ -34,11 +37,32 @@ export default {
     }),
     typescript({ tsconfig: './tsconfig.json', outputToFilesystem: true }),
     commonjs(),
+    !dev && terser(),
     copy({
       targets: [
-        { src: 'ecosystem.config.js', dest: 'dist' }
-      ],
-      hook: 'writeBundle'
-    })
+        { src: '.env', dest: 'dist' },
+        !dev && { src: 'ecosystem.config.js', dest: 'dist' },
+      ].filter(Boolean),
+      hook: 'writeBundle',
+    }),
+    !dev && generatePackageJson(),
   ],
 };
+
+function generatePackageJson(options = {}) {
+  return {
+    name: 'generate-package-json',
+    writeBundle() {
+      const packagePath = path.resolve(dir, 'package.json');
+      const distPath = path.resolve(dir, 'dist', 'package.json');
+      const originalPackageJson = JSON.parse(fs.readFileSync(packagePath));
+      const newPackageJson = {};
+      Object.keys(originalPackageJson).forEach((key) => {
+        if (!['devDependencies', 'dependencies', 'scripts'].includes(key)) {
+          newPackageJson[key] = originalPackageJson[key];
+        }
+      });
+      fs.writeFileSync(distPath, JSON.stringify(newPackageJson, null, 2));
+    },
+  };
+}
